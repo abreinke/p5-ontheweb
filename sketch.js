2d27f4a0369ee15b1fc25db807d56485574dd682
abreinke <42782067+abreinke@users.noreply.github.com>
2019-12-15 20:53:55 -0800

    changes

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let img;
+let imgTwo;
+let imgThree;
+let imgFour;
+function preload() {
+  myImage = loadImage('text1.png');
+  imgTwo = loadImage('text2.png');
+  imgThree = loadImage('text3.png');
+  imgFour = loadImage('text4.png');
+}
+
+function mousePressed() {
+  if (mousePressed == true){
+  loadImage(img);
+  }
+}
